2cc6743ffd5c1123e9846f2c36e1b3da8ca71a0b
Your Name <you@example.com>
2020-09-23 13:15:04 +0530

    commited

---
+++ app.js
@@ -0,0 +1,94 @@
+const express = require('express');
+//to find the path
+const geocoding = require('../weather/app')
+const path = require('path')
+const app = express()
+const fileDir=__dirname;
+//to find the path to edit
+
+const newPath= path.join(fileDir,'/templates')
+const otherPath= path.join(fileDir,'/parcials')
+const forecast = require('../weather/forecast')
+
+const hbs = require('hbs')
+console.log(otherPath)
+//making a static file
+app.use(express.static(newPath))
+
+//using to use common
+hbs.registerPartials(otherPath)
+
+
+//setting the templates
+app.set('view engine','hbs')
+app.set('views',newPath)
+
+
+
+
+
+app.get('',(req,res)=>{
+   res.render('index')
+})
+
+//the weather for a location 
+app.get('/weather',(req,res)=>{
+
+if(req.query.place){
+  geocoding(req.query.place,(error,data)=>{
+ if(error){
+   console.log('error')
+ }else{
+     forecast(req.query.place, (error1, data2) => {
+if(error1){
+  res.send({
+          
+  error:'please search a valid location'
+  })
+}else{
+  res.send({
+          
+    place:data.location,
+    data:data2.temperature,
+    weather:data2.weather,
+  
+  })
+}
+
+     
+     
+       })
+      }
+      
+ })
+
+}else{
+  res.send({
+    error:'please provide a location'
+  })
+
+}
+
+})
+// about page
+app.get('/about',(req,res)=>{
+   res.render('about',{
+     about:'about'
+
+   })
+ })
+ app.get('/help',(req,res)=>{
+  res.render('help')
+    
+ })
+ app.get('/help/*',(req,res)=>{
+   res.render('help')
+     
+  })
+ app.get('*',(req,res)=>{
+   res.render('error')
+     
+  })
+app.listen(3000,()=>{
+    console.log('here is it!')
+})
---
+++ templates/js/fetch.js
@@ -0,0 +1,31 @@
+
+let input = document.querySelector('.inp')
+
+document.querySelector('.weather').addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+
+    let location = input.value
+    let url =fetch(`http://localhost:3000/weather?place=${location}`)
+   let elem= document.querySelector('.para')
+if(location){
+    let text='loading...'
+elem.textContent=text
+url.then(res=>{
+res.json().then(data=>{
+if(data.error){
+    document.querySelector('.para').insertAdjacentHTML('beforeend',data.error)   
+}else{
+   elem.textContent=''
+ text = ` From ${data.place} <br>The temperatue of today is: ${data.data}. <br>Right now ,the weather : ${data.weather}  `
+elem.insertAdjacentHTML('beforeend',text)
+
+}
+     
+    
+ 
+})
+})
+    
+}
+})
